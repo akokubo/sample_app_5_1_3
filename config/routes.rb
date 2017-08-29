f4a9e4fed13686142de86233765cdb75d2cde1b3
Atsushi Kokubo <kokubo@hi-tech.ac.jp>
2017-08-29 06:10:52 +0000

    Add user following

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,15 @@ Rails.application.routes.draw do
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
-  resources :users
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
   # ユーザーの有効化を行うリソース
   resources :account_activations, only: [:edit]
   # パスワード再設定を行うリソース(newとcreateがエントリー、editとupdateが再設定)
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :microposts,          only: [:create, :destroy]
+  resources :relationships,       only: [:create, :destroy]
 end
