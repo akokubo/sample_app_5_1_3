bbd747825dfa464c9eaa03a9494adad364d616ce
Atsushi Kokubo <kokubo@hi-tech.ac.jp>
2017-08-30 04:23:33 +0000

    Add exercises 7

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get    '/about',   to: 'static_pages#about'
   get    '/contact', to: 'static_pages#contact'
   get    '/signup',  to: 'users#new'
+  # 登録に失敗した場合にも/usersにリダイレクトするのでなく、/signupにするために
+  post   '/signup',  to: 'users#create'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -11,12 +11,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
-      post users_path, params: { user: { name: "",
+      post signup_path, params: { user: { name: "",
                                          email: "user@invalid",
                                          password:              "foo",
                                          password_confirmation: "bar" } }
     end
     assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
+    # /usersでなく、/signupに送信するフォームかを検証
+    assert_select 'form[action="/signup"]'
   end
 
   test "valid signup information with account activation" do
@@ -49,6 +53,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert is_logged_in?
     follow_redirect!
     assert_template 'users/show'
+    assert_not flash.empty?
     assert is_logged_in?
   end
 end
